c46e3b29300dbab2e06d565f82cf6f02d9d87b78
OnyeEbube <okontaonyeka@gmail.com>
2023-05-09 11:29:59 +0000

    Correcting file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,16 @@ A function that queries the Reddit API and returns the number of subscribers
 for a given subreddit
 """
 import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
     If an invalid subreddit is given, the function returns 0
     """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
