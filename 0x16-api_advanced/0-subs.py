26caa20c36319d9c82cc4b6c818b8cf99c2d4a29
OnyeEbube <okontaonyeka@gmail.com>
2023-05-09 11:23:46 +0000

    Adding 0-subs.py file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    If an invalid subreddit is given, the function returns 0
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
