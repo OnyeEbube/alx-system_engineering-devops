2911ec6ef2dc3bcbadb2df79c8aeeaf37070b700
OnyeEbube <okontaonyeka@gmail.com>
2023-05-09 15:08:23 +0000

    Adding corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-A function that queries the Reddit API and returns the number of subscribers
-for a given subreddit
+number of subscribers for a given subreddit
 """
 
 from requests import get
@@ -9,18 +8,20 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-    If an invalid subreddit is given, the function returns 0
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    user_agent = {"User-Agent": "Mozilla/5.0"}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
-    data = response.json()
+    results = response.json()
 
     try:
-        return data.get('data').get('subscribers')
+        return results.get('data').get('subscribers')
+
     except Exception:
         return 0
